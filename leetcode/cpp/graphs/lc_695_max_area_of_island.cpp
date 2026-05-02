2b2309999dbe73211affe2cc92884dd62bed31d8
Ridvik Pal <ridvikpal@gmail.com>
2026-05-01 20:34:30 -0400

    Added solution for leetcode 695 in cpp

---
+++ leetcode/cpp/graphs/lc_695_max_area_of_island.cpp
@@ -0,0 +1,96 @@
+#include <vector>
+#include <array>
+#include <queue>
+
+/**
+ * https://leetcode.com/problems/max-area-of-island/description/
+ *
+ * Time complexity: O(n * m) where n = grid row length, m = grid col length
+ * Space complexity: O(n * m) where n = grid row length, m = grid col length
+ */
+class Solution {
+public:
+    // create a helper function to compute the area using bfs
+    int bfs(
+        const std::pair<int, int>& coords,
+        std::vector<std::vector<int>>& grid,
+        const int& ROW_LEN,
+        const int& COL_LEN
+    ) {
+        // init our area to 1
+        int area = 1;
+
+        // init our bfs queue
+        std::queue<std::pair<int, int>> bfsQueue;
+        // add the first coordinates to our bfs queue
+        bfsQueue.push(std::make_pair(coords.first, coords.second));
+        // mark the first coordinates as visited by setting them as 0
+        grid[coords.first][coords.second] = 0;
+
+        // setup the directions we can search in
+        const std::array<std::pair<int, int>, 4> directions{{
+            {-1, 0},
+            {1, 0},
+            {0, 1},
+            {0, -1}
+        }};
+
+        // standard bfs loop until the queue is empty
+        while (!bfsQueue.empty()) {
+            // get the first element from the bfs queue
+            const auto [row, col] = bfsQueue.front();
+            // remove the first element from the bfs queue
+            bfsQueue.pop();
+
+            // loop through all directions
+            for (const auto& [dx, dy]: directions) {
+                // get the adjacent cell coordinates
+                const int adjRow = row+dx, adjCol = col+dy;
+
+                // if
+                // 1) the adjacent cell is within grid bounds
+                // 2) the adjacent cell is 1
+                // then add it to the bfs queue, mark it as visited
+                // and increment the area count
+                if (
+                    (0 <= adjRow && adjRow < ROW_LEN) &&
+                    (0 <= adjCol && adjCol < COL_LEN) &&
+                    (grid[adjRow][adjCol] == 1)
+                ) {
+                    bfsQueue.push(std::make_pair(adjRow, adjCol));
+                    grid[adjRow][adjCol] = 0;
+                    ++area;
+                }
+            }
+        }
+
+        // finally return the area
+        return area;
+    }
+
+    int maxAreaOfIsland(std::vector<std::vector<int>>& grid) {
+        // get the grid row and col lengths
+        const int ROW_LEN = grid.size();
+        const int COL_LEN = grid[0].size();
+
+        // init our max area
+        int maxArea = 0;
+
+        // loop through all rows
+        for (int row = 0; row < ROW_LEN; ++row) {
+            // loop through all cols
+            for (int col = 0; col < COL_LEN; ++col) {
+                // if the current cell contains 1
+                // compute the area using our bfs function
+                // and update the max area accordingly
+                if (grid[row][col] == 1) {
+                    int area = bfs(std::make_pair(row, col), grid, ROW_LEN, COL_LEN);
+                    maxArea = std::max(maxArea, area);
+                }
+            }
+        }
+
+        // finally return the max area
+        return maxArea;
+    }
+};
